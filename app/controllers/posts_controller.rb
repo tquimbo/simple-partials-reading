76a5acb4232aa8f2e388f46da5052f5a814e04d9
Tim Quimbo <tquimbo1@gmail.com>
2021-10-20 16:30:52 -0400

    commit tree 800483bfa1f8af8f33ce7b7b7a5cdbe825d37540

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
+  
   def show
     @post = Post.find(params[:id])
+    @author = @post.author
   end
+  
 
   def new
     @post = Post.new
